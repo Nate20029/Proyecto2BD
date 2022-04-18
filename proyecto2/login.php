9f5a924094eef4fe1c3eb00084d74853c5d273d7
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2022-04-18 09:46:47 -0600

    login que crea cookies
    cookies para el contador de sesiones incorrectas y le manda alerta y la cookie para saber que correo esta iniciado sesion

--- proyecto2/login.php
+++ proyecto2/login.php
@@ -4,13 +4,19 @@ include_once ("conexion.php");
 $conn = CConexion::ConexionBD();
 
 $bandera = 0;
+$cont = 1;
 
 if(isset($_POST['btentrar'])){
     $correo = $_POST['correoI'];
     $contra = $_POST['contra'];
 	
-    }
-  
+	if( isset($_COOKIE["contadorCK"]) ){
+		setcookie("contadorCK",$_COOKIE["contadorCK"] + 1 , time() + 30*24*60*60);
+	}
+	else{
+		setcookie("contadorCK", 1, time() + 30*24*60*60);
+	}
+}
     //contraseña hash
 $pass_hash = md5($contra);
 
@@ -26,16 +32,27 @@ if ($publishers) {
 	// show the publishers
 	foreach ($publishers as $publisher) {
 		if($publisher['pass_hash'] == $pass_hash AND $publisher['email'] == $correo){
-			include_once ("principal.php");
+			setcookie("correoCK", $correo, time() + 30*24*60*60);
 			$bandera = 1;
+			setcookie("contadorCK", 1, time() - 30*24*60*60);
+			$cont = 1;
+			include_once ("principal.php");
 		}
 
 	}
 	if($bandera != 1)
 	{
-		include_once ("inicio.php");
+		setcookie("correoCK", "correo", time() - 30*24*60*60);
 		$bandera = 0;
-		echo "<script>alert('Correo o contraseña incorrecta');</script>";
+		$cont = $_COOKIE["contadorCK"]+1;
+		echo "<script>alert('Correo o contraseña incorrecta, Intentos: $cont');</script>";
+		if($cont >= 3){
+			include_once ("maxIntentos.php");
+		}
+		else{
+			include_once ("inicio.php");
+		}
+		
 	}
 }
 
